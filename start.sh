b30949ba00c4ae59e857181839f249a903afae2e
aagustinconti <aagustinconti@gmail.com>
2022-11-13 21:31:42 -0300

    Little changes

--- start.sh
+++ start.sh
@@ -28,13 +28,18 @@
 
 
 # Create the image of torch
-cd ./dependencies_files
+
+echo "Building the Docker Image..."
+
+cd ./dependencies
 docker build -t yolov7_detect_track_count .
 cd ..
 
 # And then run
 # 1. To allow the use of screen
+echo "Setting up X Server to accept connections. Turning Access control off..."
 xhost +
 
 # 2. To run the image
+echo "Runing the Docker Image in the current location -> Display: ON ; Web camera access: ON"
 docker run --gpus all --rm -it -e DISPLAY=$DISPLAY -v  $PWD:/workspace -v /tmp/.X11-unix:/tmp/.X11-unix:rw --device="/dev/video0:/dev/video0"  yolov7_detect_track_count:latest
